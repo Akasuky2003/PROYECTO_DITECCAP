40d54b78e1fe8df6deffe35b84b8eec1bbefa59e
axvg <91773368+axvg@users.noreply.github.com>
2023-02-05 16:10:00 -0500

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer({}) {
+  return (
+    <div className="footer">
+      <div className="">
+        <span className="text-sm">Powered by</span>
+        <img src="logo.png" />
+      </div>
+    </div>
+  );
+}
