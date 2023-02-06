0551aff19df9e29fae1c1f5920baf7e469ef6ec8
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2023-02-06 10:03:36 -0500

    add api/payment

--- services/twilio/index.ts
+++ services/twilio/index.ts
@@ -8,7 +8,7 @@ const client = twilio(
 export default async function sendSMS(phoneNumber: string, transaction_amount: number) {
     try {
         const message = client.messages.create({
-            body: `Estimado(a), tu pago de S/.${transaction_amount} en Creativos Pariona se realizo con exito y empezaremos a preparar tu pedido. Gracias por tu preferencia.`,
+            body: `Estimado(a), tu pago por S/.${transaction_amount} en Creativos Pariona se realizo con exito y empezaremos a preparar tu pedido. Gracias por tu preferencia.`,
             to: phoneNumber,
             from: "+15392455564"
         });
