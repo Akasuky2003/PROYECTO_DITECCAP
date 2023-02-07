52252e15f1793bf1e9f067889e2e1cae2be4f703
Gian <gianfrancopariona9@gmail.com>
2023-02-07 17:53:48 -0500

    index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-import Link from 'next/link'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
-import Image from "next/image";
+
 
 
 const inter = Inter({ subsets: ['latin'] })
