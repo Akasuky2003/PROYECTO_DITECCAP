711a505d7ca39c503a6a7f318f834260ae72b923
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2023-02-11 17:00:12 -0500

    add title home

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,16 @@ import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Proyecto DITECCAP</title>
+    </Head>
       <div className={styles.container}>
         <Navbar />
         <img style={{height:"auto"}} src="/Paginaprincipal.png" alt="Imagen" />
